521845fd2d003a201baf8dfa4108d8a2d95cfa97
Rafael Guedes <v-gueder-ave@ae.com>
2014-03-27 09:26:02 -0400

    Tweaks on presentation files

--- js/scripts.js
+++ js/scripts.js
@@ -4640,7 +4640,7 @@ Reveal.addEventListener('ready', function() {
   'use strict';
 
   var footer = TEMPLATES.footer({
-    title: 'Talk template'
+    title: 'AngularJS - Part 2'
   });
 
   var sectionEls = document.querySelectorAll('section:not(.stack)');
